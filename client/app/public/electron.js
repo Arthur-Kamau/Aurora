67b83f39a78ee964b78901cc908920c9c4000d77
Arthur_Kamau <kamaukenn11@gmail.com>
2020-04-15 09:25:43 +0300

    app updater bug fixes

--- client/app/public/electron.js
+++ client/app/public/electron.js
@@ -1,12 +1,12 @@
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const Menu = electron.Menu;
-const MenuItem = electron.MenuItem;
+// const Menu = electron.Menu;
+// const MenuItem = electron.MenuItem;
 const path = require("path");
 const isDev = require("electron-is-dev");
 const ipcMain = electron.ipcMain;
-
+const { autoUpdater } = require('electron-updater');
 
 
 let mainWindow;
@@ -23,17 +23,17 @@ function createWindow() {
     });
 
     //on minimize small window pop up
-    mainWindow.once('focus', () => mainWindow.flashFrame(false))
-    mainWindow.flashFrame(true)
-
+    mainWindow.once('focus', () => mainWindow.flashFrame(false));
+    mainWindow.flashFrame(true);
+    mainWindow.setMenuBarVisibility(false);
     // menu item
-    const template = [{
-        label: '',
-        submenu: []
-    }]
-    // menu 
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu)
+    // const template = [{
+    //     label: '',
+    //     submenu: []
+    // }]
+    // // menu 
+    // const menu = Menu.buildFromTemplate(template);
+    // Menu.setApplicationMenu(menu)
 
 
     mainWindow.loadURL(
@@ -42,7 +42,12 @@ function createWindow() {
             `file://${path.join(__dirname, "../build/index.html")}`
     );
     //dev tools
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.once('ready-to-show', () => {
+        autoUpdater.checkForUpdatesAndNotify();
+      });
+
     mainWindow.on("closed", () => (mainWindow = null));
 }
 app.on("ready", createWindow);
@@ -83,10 +88,10 @@ ipcMain.on('OpenFile', (event, arg) => {
     // }
 });
 
-function openModal() {
+function openModal(url) {
     const { BrowserWindow } = require('electron');
     let modal = new BrowserWindow({ parent: mainWindow, modal: true, show: false })
-    modal.loadURL('https://www.sitepoint.com')
+    modal.loadURL(url)
     modal.once('ready-to-show', () => {
         modal.show()
     })
@@ -95,3 +100,26 @@ function openModal() {
 
 
 
+
+ipcMain.on('app_version', (event) => {
+
+    console.log("app version " + app.getVersion());
+    event.sender.send('app_version', { version: app.getVersion() });
+  });
+  ipcMain.on('restart_app', () => {
+    app.relaunch()
+    app.exit()
+  });
+  ipcMain.on('restart_app_install_update', () => {
+    autoUpdater.quitAndInstall();
+  
+  });
+  
+  
+  autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update_available');
+  });
+  
+  autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('update_downloaded');
+  });
